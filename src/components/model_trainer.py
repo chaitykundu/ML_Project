7195e070ef88ddb45034fabd907541854e487ec2
Chaity Kundu <chaitykundu323@gmail.com>
2025-07-15 10:15:54 +0600

    MOdel trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,3 +18,18 @@ from src.exception import CustomException
 from src.logger import logging
 
 from src.utils import save_object,evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path= os.path.join("artifacts","model.pkl")
+
+class ModelTrainer:
+    def __initi__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+        try:
+            logging.info("Splitting training and testing input data")
+            
+        except:
+            pass
